b7a6ac4b4b79a7d55056bd20cc6fed2817c5d923
subsir <tiancaizhangdaxian@sjtu.edu.cn>
2024-07-30 00:28:19 +0800

    size = 512

--- src/rs.hpp
+++ src/rs.hpp
@@ -3,7 +3,7 @@
 #include "register.h"
 #include "tools.h"
 #include <array>
-const int RS_SIZE = 1024;
+const int RS_SIZE = 512;
 struct RS_Input {
   Wire<1> rob_rs_get_in; // rs 获得了值设置为0
   Wire<1> rob_error;
